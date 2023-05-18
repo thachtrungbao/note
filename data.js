b60e7c364c72e3411b68226fe940cb71c9c30cf0
bao <thachtrungbao4@gmail.com>
2023-05-18 11:07:49 +0700

    update lan 2

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ myInput.addEventListener("change", () => {
   console.log(myInput.value);
 });
 
-const public = [
+var publicData = [
   {
     id: 1,
     Image: "./assets/img/content-img.jpg",
@@ -54,20 +54,90 @@ const public = [
   },
 ];
 
-const myContent = document.querySelector(".tab-content");
+var privateData = [
+  {
+    id: 1,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 1",
+    content: "private1",
+  },
+  {
+    id: 2,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 2",
+    content: "private2",
+  },
+  {
+    id: 3,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 3",
+    content: "private3",
+  },
+  {
+    id: 4,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 4",
+    content: "private4",
+  },
+  {
+    id: 5,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 5",
+    content: "private5",
+  },
+  {
+    id: 6,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 6",
+    content: "private6",
+  },
+  {
+    id: 7,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 7",
+    content: "private7",
+  },
+  {
+    id: 8,
+    Image: "./assets/img/private-img.jpg",
+    title: "private 8",
+    content: "private8",
+  },
+];
 
-const showInHtml = public.map((project, index) => {
-  return `
-                  <div class="tab-content-icon-item">
-                  <img src=${project.Image} alt="" class="content-icon-img"/>
-                  <h5 class="icon-heading"> ${project.title}</h5>
-                  <div id="hide">
-                    <p class="content">${project.content}</p>
-                  </div>
-                </div> `;
-});
+// Map
+var publicContainer = document.getElementById("public-container");
+publicContainer.innerHTML = publicData
+  .map((data) => {
+    return `
+  <div class="tab-content-icon-item">
+  
+    <img src="${data.Image}" alt="" class="content-icon-img"/>
 
-myContent.innerHTML = showInHtml;
+    <h5 class="icon-heading">${data.title}</h5>
+    <p class="content">${data.content}</p>
+  </div>
+</div>
+  `;
+  })
+  .join("");
+
+var privateContainer = document.getElementById("private-container");
+privateContainer.innerHTML = privateData
+  .map((data) => {
+    return `
+    <div class="private-content-item">
+
+    <img src="${data.Image}" alt="" class="private-img"/>
+
+    <h5 class="private-heading">${data.title}</h5>
+    <p class="private-content">${data.content}</p>
+  </div>
+</div>
+  `;
+  })
+  .join("");
+
 const iconContainer = document.querySelector(".tab-content");
 const sortIconButton = document.querySelector(".sort-icon");
 const sortListButton = document.querySelector(".sort-list");
@@ -77,17 +147,35 @@ function showIconsHorizontal() {
   iconContainer.classList.add("horizontal");
   iconContainer.classList.remove("vertical");
 
-  const showInHtml = public.map((project, index) => {
-    return `
+  publicContainer.innerHTML = publicData
+    .map((data) => {
+      return `
+  <div class="tab-content-icon-item">
+  
+    <img src="${data.Image}" alt="" class="content-icon-img"/>
 
-                  <div class="tab-content-icon-item">
-                    <img src=${project.Image} alt="" class="content-icon-img"/>
-                    <h5 class="icon-heading"> ${project.title}</h5>
-                  <p class="content">${project.content}</p>
-                  </div> `;
-  });
+    <h5 class="icon-heading">${data.title}</h5>
+    <p class="content">${data.content}</p>
+  </div>
+</div>
+  `;
+    })
+    .join("");
 
-  myContent.innerHTML = showInHtml;
+  privateContainer.innerHTML = privateData
+    .map((data) => {
+      return `
+    <div class="private-content-item">
+  
+    <img src="${data.Image}" alt="" class="private-img"/>
+
+    <h5 class="private-heading">${data.title}</h5>
+    <p class="private-content">${data.content}</p>
+  </div>
+</div>
+  `;
+    })
+    .join("");
 }
 
 // Hàm hiển thị các icon theo hàng dọc
@@ -97,6 +185,11 @@ function showIconsVertical() {
   const iconItems = document.querySelectorAll(".tab-content-icon-item");
   const image = document.querySelectorAll(".tab-content-icon-item img");
   const iconheading = document.querySelectorAll(".icon-heading");
+
+  const privateItem = document.querySelectorAll(".private-content-item");
+  const privateImg = document.querySelectorAll(".private-content-item img");
+  const privateHeading = document.querySelectorAll(".private-heading");
+  const privateContent = document.querySelectorAll(".private-content");
 
   iconItems.forEach((item) => {
     item.style.width = "1400px";
@@ -112,6 +205,24 @@ function showIconsVertical() {
     item.style.marginTop = "-5px";
     item.style.border = "5px solid #e5e5e5";
   });
+
+  privateItem.forEach((item) => {
+    item.style.width = "1400px";
+    item.style.height = "60px";
+  });
+  privateHeading.forEach((item) => {
+    item.style.marginLeft = "65px";
+    item.style.marginTop = "-50px";
+  });
+  privateImg.forEach((item) => {
+    item.style.width = "50px";
+    item.style.height = "50px";
+    item.style.marginTop = "-5px";
+    item.style.border = "5px solid #e5e5e5";
+  });
+  privateContent.forEach((item) => {
+    item.style.marginLeft = "65px";
+  });
 }
 
 // click "sort-icon"
@@ -124,13 +235,20 @@ sortListButton.addEventListener("click", () => {
   showIconsVertical();
 });
 
-//filter
+//filter;
 var searchInput = document.querySelector("#myInput");
 searchInput.addEventListener("input", function (e) {
   let txtSearch = e.target.value.trim();
-  let listProductDOM = document.querySelectorAll(".tab-content-icon-item");
-  listProductDOM.forEach((item) => {
-    // console.log(item.innerText);
+  let listPublicDOM = document.querySelectorAll(".tab-content-icon-item");
+  listPublicDOM.forEach((item) => {
+    if (item.innerText.includes(txtSearch)) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+  let listPrivateDOM = document.querySelectorAll(".private-content-item");
+  listPrivateDOM.forEach((item) => {
     if (item.innerText.includes(txtSearch)) {
       item.classList.remove("hide");
     } else {
@@ -138,3 +256,23 @@ searchInput.addEventListener("input", function (e) {
     }
   });
 });
+
+//event tab
+var privateContainer = document.getElementById("private-container");
+var publicContainer = document.getElementById("public-container");
+
+const publicButton = document.querySelector(".public");
+const privateButton = document.querySelector(".private");
+
+privateButton.onclick = () => {
+  privateContainer.style.display = "flex";
+  publicContainer.style.display = "none";
+  privateButton.classList.add("active");
+  publicButton.classList.remove("active");
+};
+publicButton.onclick = () => {
+  privateContainer.style.display = "none";
+  publicContainer.style.display = "flex";
+  privateButton.classList.remove("active");
+  publicButton.classList.add("active");
+};
